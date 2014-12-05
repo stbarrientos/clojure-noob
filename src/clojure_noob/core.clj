f7e288598343d941b67a274f12f1838c7a05f8a3
stbarrientos <stbarrientos@gmail.com>
2014-12-05 13:00:45 -0500

    finished fizzbuzz

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,7 +1,23 @@
 (ns clojure-noob.core
   (:gen-class))
 
+(defn fizzBuzz
+  [number]
+  (if (= 0 (mod number 3))
+    (if (= 0 (mod number 5))
+      (println "fizzbuzz")
+      (println "buzz")
+      )
+    (if (= 0 (mod number 5))
+      (println "fizz")
+      (println number))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "I'm A Little Teapot!")
+  (def x 1)
+  (while (< x 101)
+    (do (fizzBuzz x)
+    (def x (+ x 1)))
+    ))
